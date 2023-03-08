459fc3147c1979fbc1366d4125235e28f51e9dd4
Guyokps <124910053+Guyokps@users.noreply.github.com>
2023-03-08 17:55:25 +0200

    Add files via upload

---
+++ MainGame.py
@@ -0,0 +1,24 @@
+import GuessGame
+import MemoryGame
+import CurrencyRouletteGame
+from Live import load_game, welcome
+
+welcome()
+
+play_again = "y"
+
+while play_again.lower() == "y":
+    user_choices = load_game()
+
+    if user_choices[0] == 2:
+        GuessGame.play(user_choices[1])
+    elif user_choices[0] == 1:
+        MemoryGame.play(user_choices[1])
+    elif user_choices[0] == 3:
+        CurrencyRouletteGame.play(user_choices[1])
+
+    play_again = input("Do you want to play again? (y/n):\n")
+
+print("Thanks for playing!")
+
+
